8824848b09fce902a2eb0e25c15c39ee3788039a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-13 00:40:39 -0300

    printing movies list

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Olá, mundo! Meu projeto Python está funcionando.")
+import pandas as pd
+
+# Carregar dados do MovieLens (100k)
+movies = pd.read_csv("https://grouplens.org/datasets/movielens/32m/ml-25m/movies.csv")
+
+print(movies.head())
